55072429f7ee8960ac7cb116ef79ecb4aab7be8a
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-04-06 00:10:32 +0800

    spring and camera

--- my-app/src/lib/Components/Scene.svelte
+++ my-app/src/lib/Components/Scene.svelte
@@ -1,8 +1,26 @@
 <script lang="ts">
     import { T } from '@threlte/core'
+    import { interactivity } from '@threlte/extras'
+    import { spring } from 'svelte/motion'
+
+    interactivity()
+    const scale = spring([1,1,1])
 </script>
 
-<T.Mesh position.y={1}>
-    <T.BoxGeometry args={[1, 2, 1]}/>
-    <T.MeshBasicMaterial color="hotpink"/>
+<T.Mesh 
+    position={[0,0,0]}
+    scale={$scale}
+    on:pointerenter={() => scale.set([2,0.5,2])}
+    on:pointerleave={() => scale.set([1,1,1])}
+    >
+    <T.BoxGeometry args={[2, 2, 2]}/>
+    <T.MeshBasicMaterial color="green"/>
 </T.Mesh>
+
+<T.PerspectiveCamera 
+    makeDefault
+    position={[5,5,5]}
+    on:create={({ ref }) => {
+        ref.lookAt(0, 0, 0)
+    }}
+/>
